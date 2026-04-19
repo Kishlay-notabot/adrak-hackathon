a67aeddd42ea719f9f4904ee497c5827387454ad
Savitender <paramsavi06@gmail.com>
2026-04-19 20:01:36 +0530

    Added Mobile CORS Origin

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}))
 app.use(express.json());
 
 // ── Routes ──────────────────────────────────────────────────────────
